4d1593687cbeacb5ff1839e652629df2dd218057
HEEJU KIM <hiz@Hizs-MacBook-Pro.local>
2019-12-24 13:24:14 +0900

    middle

--- src/server/helpers/fetch.js
+++ src/server/helpers/fetch.js
@@ -1,8 +1,33 @@
 const https = require('https');
 
 async function retrieveArticle(url) {
-  // TODO: retrieve the html string from given url and return as promise
+  return new Promise((resolve, reject) => {
+    https.get(url, res => {
+
+      const resData = [];
+      res.on('data', chunk => {
+        console.log(chunk)
+        resData.push(chunk)
+      })
+      .on('end', () => {
+        const resultData = resData.join()
+        console.log(resultData)
+        resolve('hoho')
+      })
+    })
+    .on('error', (err) => {
+      reject(err)
+    })
+  })
 }
+
+retrieveArticle('https://www.google.com/')
+.then((msg) => {
+  console.log(msg)
+})
+.catch(err => {
+  console.error(err)
+})
 
 module.exports = {
   retrieveArticle
